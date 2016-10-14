a8fdeefd59ee75b85e392f430eb3902db9119b3c
Simon Layfield <simonlayfield@gmail.com>
2016-10-14 15:27:28 +1100

    Added basic band name generator

---
+++ work/band-name-generator/js/main.js
@@ -0,0 +1,59 @@
+
+
+var oneWords = ['saint', 'stone', 'fray', 'bay', 'lake', 'break', 'kid', 'fire', 'slate', 'wake', 'trip', 'tape', 'park', 'fair', 'trade', 'cross', 'war', 'cold', 'part', 'march', 'tide', 'may', 'way', 'mark', 'wild', 'line', 'suns', 'sail', 'trace', 'light', 'raid', 'plan', 'bond', 'shore', 'day'];
+
+var twoWords =   ['future', 'amber', 'tiger', 'china', 'border', 'river', 'cinder', 'armour', 'raven', 'reader', 'aero', 'fever', 'taper', 'savour', 'circle', 'karma', 'pilot', 'mirror', 'awake', 'service', 'skyline', 'silence', 'silver', 'zebra', 'echo', 'haven', 'idle'];
+
+var threeWords = ['memory', 'kindling', 'origin', 'radio', 'union'];
+
+var latch = true,
+minimum = 0,
+maximum = 10;
+
+window.onload = function () {
+    var randomnumber = Math.floor(Math.random() * (maximum - minimum)) + minimum;
+    document.getElementById('one').innerText = oneWords[randomNumber(maximum)];
+    document.getElementById('two').innerText = twoWords[randomNumber(maximum)];
+}
+
+function randomNumber (maximum) {
+
+    return Math.floor(Math.random() * (maximum - minimum)) + minimum;
+}
+
+function switchWord (word) {
+
+    if (word === 'the') {
+
+        if (latch) {
+            document.getElementById('the').classList.remove('active');
+            latch = false;
+        } else {
+            document.getElementById('the').classList.add('active');
+            latch = true;
+        }
+
+    }
+
+    if (word === 'one') {
+
+        var el = document.getElementById('one');
+        el.classList.remove('active');
+        setTimeout(function(){
+            el.innerText = oneWords[randomNumber(oneWords.length)];
+            el.classList.add('active');
+        }, 200);
+
+    } else if (word === 'two') {
+
+        var el = document.getElementById('two');
+        el.classList.remove('active');
+        setTimeout(function(){
+            el.innerText = twoWords[randomNumber(twoWords.length)];
+            el.classList.add('active');
+        }, 200);
+
+
+    }
+
+}
